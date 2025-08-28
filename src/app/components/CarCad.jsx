781cf21154c8bb7f5575312147402bc2df0ef9b8
jhonatan gutierrez <wolftareas@gmail.com>
2025-08-27 20:21:43 -0500

    listo mira hay estan los cambios

--- src/app/components/CarCad.jsx
+++ src/app/components/CarCad.jsx
@@ -4,7 +4,27 @@ import Link from "next/link";
 export default function CarCard({ id, marca, modelo, precio, imagen }) {
   return (
     <div className="bg-white border rounded-xl p-4 shadow-md hover:shadow-lg transition">
-      <Image src={imagen} alt={`${marca} ${modelo}`} width={300} height={200} className="rounded-lg" />
+      <Image 
+        src="/Carro1.jpg"
+        alt={`${marca} ${modelo}`} 
+        width={300} 
+        height={200} 
+        className="rounded-lg object-cover" 
+      />
+       <Image 
+        src="/Carro2.jpg"
+        alt={`${marca} ${modelo}`} 
+        width={300} 
+        height={200} 
+        className="rounded-lg object-cover" 
+      />
+       <Image 
+        src="/Carro3.jpg"
+        alt={`${marca} ${modelo}`} 
+        width={300} 
+        height={200} 
+        className="rounded-lg object-cover" 
+      />
       <h2 className="text-xl font-bold mt-2">{marca} {modelo}</h2>
       <p className="text-gray-700">Precio: ${precio}</p>
       <Link href={`/autos/${id}`} className="text-blue-500 hover:underline">
